2fa2bff005e2e3fdc78ba54d72414a7ea27ae53c
lucopdev <lucopdev@gmail.com>
2023-12-15 21:56:10 -0300

    requisito 3

--- codetank/src/Main.java
+++ codetank/src/Main.java
@@ -1,9 +1,17 @@
 import com.ada.Requisito1;
+import com.ada.Requisito2;
+import com.ada.Requisito3;
 
 public class Main {
 
   public static void main(String[] args) {
-    System.out.println(Requisito1.calculoEmprestimo());
+//    Requisito 1 - descomente a linha de baixo para testar
+//    System.out.println(Requisito1.calculoEmprestimo());
 
+//    Requisito 2 - descomente a linha de baixo para testar
+//    System.out.println(Requisito2.conversorTemperatura());
+
+//    Requisito 3 - descomente a linha de baixo para testar
+    System.out.println(Requisito3.triangulo());
   }
 }
--- codetank/src/com/ada/Requisito3.java
+++ codetank/src/com/ada/Requisito3.java
@@ -1,5 +1,38 @@
 package com.ada;
 
+import java.util.Scanner;
+
 public class Requisito3 {
 
+  public static String triangulo() {
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.print("Digite o tamanho da base da pirâmide [número impar]: ");
+    int base = scanner.nextInt();
+
+    if (base % 2 == 0) {
+        base++;
+    }
+
+    System.out.println();
+
+    int meio = (base / 2) + 1;
+
+    for (int i = 1; i <= meio; i++) {
+      for (int j = 1; j <= meio - i; j++) {
+        System.out.print(" ");
+      }
+
+      for (int k = 1; k <= (2 * i) - 1; k++) {
+        System.out.print("*");
+      }
+      System.out.println();
+    }
+
+    System.out.println();
+    return String.format("Primrâmide de base %d", base);
+  }
 }
+
+
+
